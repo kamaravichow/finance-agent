e58939cb6e21f1f12874aee034a729aa486bab50
Aravind Chowdary <kamaravichow@gmail.com>
2026-01-19 12:06:03 -0500

    fix: correct YFinanceTools initialization parameters

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from agno.os import AgentOS
 from agno.agent import Agent
 from agno.db.sqlite import SqliteDb
-from agno.models.anthropic import Claude
+from agno.tools.reasoning import ReasoningTools
+from agno.tools.yfinance import YFinanceTools
+from agno.models.google import Gemini
+import os
+
+import dotenv
+
+dotenv.load_dotenv(".env.local")
 
 agent = Agent(
     name="Personal Accountant",
-    model=Claude(id="claude-sonnet-4-5"),
+    model=Gemini(id="gemini-flash-latest", api_key=os.getenv("GOOGLE_API_KEY")),
+    # reasoning=True,
     db=SqliteDb(db_file="personal_accountant.db"),
     add_history_to_context=True,
+    enable_agentic_memory=True,
+    tools=[
+        ReasoningTools(add_instructions=True),
+        YFinanceTools(),
+    ],
+    cache_session=True,
     markdown=True,
+    telemetry=True,
 )
 
 agent_os = AgentOS(agents=[agent])
